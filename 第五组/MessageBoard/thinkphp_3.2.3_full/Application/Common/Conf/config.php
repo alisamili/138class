b8949508ec9c89a03f4c65fa7994a02bf26ce71b
TakingChen <447979743@qq.com>
2017-01-03 21:52:59 +0800

    第五组

---
+++ 第五组/MessageBoard/thinkphp_3.2.3_full/Application/Common/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+// 为了以后的可移植性
+return array(
+	'APP_GROUP_LIST'=>'Home',
+	'DEFAULT_GROUP'=>'Home',
+	
+	'DB_TYPE'=>'mysql',// 数据库类型
+	'DB_HOST'=>'127.0.0.1',// 服务器地址
+	'DB_NAME'=>'aoshi',// 数据库名
+	'DB_USER'=>'root',// 用户名
+	'DB_PWD'=>'',// 密码
+	'DB_PORT'=>'3306',// 端口
+);
