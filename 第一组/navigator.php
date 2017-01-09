113acfd8c49af3658239668833d1f73b7f9bc768
scientist2009 <scientist2009zh@qq.com>
2017-01-09 09:45:06 +0800

    Delete navigator.php

--- 第一组/navigator.php
+++
@@ -1,8 +0,0 @@
-<!-- 左侧导航栏 -->
-<div class="row-left col-lg-3 no_radius">
-	<div class="list-group no_radius">
-		<a href="?my_settings=1" class="list-group-item no_radius">资料设置</a>
-		<a href="?my_courses=1" class="list-group-item no_radius">我的课程</a>
-		<a href="?my_communities=1" class="list-group-item no_radius">我的社区</a>
-	</div>
-</div>
